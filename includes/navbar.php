d968721fdc35fbe8049b0a06360b6c6069193118
lor54 <contessalorenzo.dev@gmail.com>
2022-01-15 19:34:36 +0100

    Search bar added

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,6 +2,13 @@
   <div class="w3-bar w3-light-grey">
 		<img src="img/logo.png" class="w3-bar-item" width="300px" height="44px">
     <a href="index.php" class="w3-bar-item w3-button w3-green">Home</a>
+
+    <div class="search-container">
+      <form action="/search.php">
+        <input type="text" placeholder="Cerca.." name="search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </form>
+    </div>
     <!--<a href="manage.php" class="w3-bar-item w3-button w3-hide-small">Gestisci Viaggi</a>-->
     <div class="w3-dropdown-hover w3-right">
       <button class="w3-button">
