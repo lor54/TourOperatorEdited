33848aa6f485c4684f307ecd5739b0eb7398b3df
lor54 <contessalorenzo.dev@gmail.com>
2022-01-15 19:22:09 +0100

    Added user role separation

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require('db.php');
     include("auth.php");
 
-    if($_SESSION["email"] == "staff") {
+    if($_SESSION["tipo"] == "staff") {
         include "admin.php";
     } else {
         include "user.php";
