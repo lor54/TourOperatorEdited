20f6bbabed98e0eb531f798b27f748851f4098cb
lor54 <contessalorenzo.dev@gmail.com>
2022-01-15 21:31:41 +0100

    Search bar moved

--- user.php
+++ user.php
@@ -2,26 +2,35 @@
     require('db.php');
     include("auth.php");
 
-    $query = "SELECT id_viaggio, titolo, n_persone from prenotare join viaggio on prenotare.id_viaggio = viaggio.id group by titolo";
-    $result = $conn->query($query) or die($conn->sqlstate);
+	if (isset($_GET['search'])) {
+		$searchInput = $_POST['search'];
+		$query = "SELECT * from viaggio where titolo LIKE '$searchInput'";
+	} else {
+		$query = "SELECT city, address, phone FROM offices";
+	}
+	$result = $conn->query($query);
+	if (!$result) {
+		$error = $conn->error;
+	}
 
-    $rows = array();
-    while($row = $result->fetch_array(MYSQLI_ASSOC))
-    {
-        $rows[] = $row;
-    }
+        $query = "SELECT id_viaggio, titolo, n_persone from prenotare join viaggio on prenotare.id_viaggio = viaggio.id group by titolo";
+        $result = $conn->query($query) or die($conn->sqlstate);
 
-    $i = 0;
-    $titoli = array();
-    foreach($rows as $row)
-    {
-        array_push($titoli, array(
-            0 => $row['titolo'],
-            1 => (int) $row['n_persone']
-        ));
-    }
+        $rows = array();
+        while($row = $result->fetch_array(MYSQLI_ASSOC))
+        {
+            $rows[] = $row;
+        }
 
-    
+        $i = 0;
+        $titoli = array();
+        foreach($rows as $row)
+        {
+            array_push($titoli, array(
+                0 => $row['titolo'],
+                1 => (int) $row['n_persone']
+            ));
+        }
 
     //print_r($titoli);
     
@@ -39,9 +48,12 @@
     <body>
         <?php include("includes/navbar.php"); ?>
         <div class="w3-row" style="margin-top: 10%">
+            <?php if (isset($error)) {
+                echo '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'.$error.'</div>';
+            } ?>
             <div class="w3-col s7 w3-center" style="margin-left: 20%; border-style: solid;">
                 <div class="search-container">
-                    <form action="/search.php">
+                    <form action="" method="get">
                         <input type="text" placeholder="Cerca.." name="search">
                         <button type="submit"><i class="fa fa-search"></i></button>
                     </form>
